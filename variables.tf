1436471911928336cca05a3a56cb09d0b3f588f3
ddimri <deepak_dimri@hotmail.com>
2017-06-17 11:37:17 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,50 @@
+variable "home_dir" {}
+
+variable "aws_profile" {}
+
+variable "project" {}
+
+variable "environment" {}
+
+variable "vpc_cidr_block" {}
+
+variable "private_subnet1_cidr_block" {}
+
+variable "private_subnet2_cidr_block" {}
+
+variable "public_subnet1_cidr_block" {}
+
+variable "public_subnet2_cidr_block" {}
+
+variable "availability_zones" {
+  type        = "list"
+}
+
+variable "ccjumpbox_ami" {}
+
+variable "awstraining_server_ami" {}
+
+variable "awstraining_openvpn_proxy_ami" {}
+
+variable "instance_type" {}
+
+variable "key_name" {}
+
+variable "aws_region" {}
+
+variable "vpc_name" {}
+
+variable "feyedc_cidr_block" {}
+
+variable "remote_vpc_cidr_block" {}
+
+variable "private_key" {
+  description = "private key to connect to hosts from the jumpbox"
+}
+
+variable "count" {
+  default     = "1"
+  description = "the number of instances"
+}
+
+variable "ssl_certificate" {}
