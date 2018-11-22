1d10706d72e41c88cefd56edfd50bb9df926deaa
ddimri <deepak_dimri@hotmail.com>
2018-11-22 20:06:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,14 +24,14 @@ resource "aws_security_group" "awstraining-mgmt-public-subnet-sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${var.cidr_block}"]
+    cidr_blocks = ["${var.vpc_cidr_block}"]
   }
 
   ingress {
     from_port   = -1
     to_port     = -1
     protocol    = "icmp"
-    cidr_blocks = ["${var.cidr_block}"]
+    cidr_blocks = ["${var.vpc_cidr_block}"]
   }
 
   # outbound internet access
@@ -273,7 +273,7 @@ resource "aws_security_group" "awstraining-server-sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${var.cidr_block}"]
+    cidr_blocks = ["${var.vpc_cidr_block}"]
   }
 
  # outbound internet access
